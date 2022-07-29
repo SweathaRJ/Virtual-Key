b195605fd4e5c52a45e16248fd755ccec09588a7
SweathaRJ <109415590+SweathaRJ@users.noreply.github.com>
2022-07-29 12:38:29 +0530

    Main Project of Phase 1

---
+++ Virtual Key(Main Project-Phase1)/src/fileSorting/LockedMeMain.java
@@ -0,0 +1,15 @@
+package fileSorting;
+
+public class LockedMeMain {
+	
+public static void main(String[] args) {
+		
+		// Create "main" folder if not present in current folder structure
+		FileOperations.createMainFolderIfNotPresent("Files");
+		
+		MenuOptions.printWelcomeScreen("Project", "Sweatha");
+		
+		HandleOptions.handleWelcomeScreenInput();
+	}
+
+}
